51cc893de64bee229258a8c9de6fa7d1046bda86
Jamie Vlegels <vlegels.jamie@gmail.com>
2024-02-05 20:42:56 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from pcbm.data.metashift import load_metashift_data
-train_loader, test_loader, idx_to_class = load_metashift_data(None, scenario=1, seed=42, batch_size=32, num_workers=4)
-class_to_idx = {v:k for k,v in idx_to_class.items()}
-classes = list(class_to_idx.keys())
-
-print(classes)
-print(train_loader.dataset[0], test_loader.dataset[0])
